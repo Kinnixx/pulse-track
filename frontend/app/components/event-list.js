10c84e11d04b6123ec7d16d91fb19f1010ca0c62
Kinnixx <anais.delattre1@gmail.com>
2025-04-18 18:46:30 +0200

    events loaded directly in EventList component with loading state and filtering logic

--- frontend/app/components/event-list.js
+++ frontend/app/components/event-list.js
@@ -3,14 +3,33 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class EventListComponent extends Component {
+  @tracked events = [];
   @tracked selectedType = '';
+  @tracked isLoading = true;
+
+  constructor() {
+    super(...arguments);
+    this.loadEvents();
+  }
+
+  async loadEvents() {
+    try {
+      const response = await fetch('http://localhost:8000/events');
+      const data = await response.json();
+      this.events = data;
+    } catch (error) {
+      console.error('Error fetching events:', error);
+    } finally {
+      this.isLoading = false
+    }
+  }
 
   get filteredEvents() {
     if(!this.selectedType) {
-      return this.args.events;
+      return this.events;
     }
 
-    return this.args.events.filter(event => event.type === this.selectedType);
+    return this.events.filter(event => event.type === this.selectedType);
   }
 
   @action
